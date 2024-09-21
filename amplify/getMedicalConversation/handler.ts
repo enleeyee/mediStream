5c94ce8c6a0ad77c2e52e79ff7206244b5f91b39
Enlai <enleyee@gmail.com>
2024-09-20 22:02:18 -0500

    fixed error in getMedicalConversation handler

--- amplify/getMedicalConversation/handler.ts
+++ amplify/getMedicalConversation/handler.ts
@@ -1,4 +1,6 @@
-export const handler = async (event) => {
+import type { Handler } from 'aws-lambda';
+
+export const handler: Handler = async (event, context) => {
     return "Hello from my first function!";
   };
   
