f1034c446644ea6bb769538a58815411ef19bb4d
Nancy Luu <139261816+helloworldmynameisnancy@users.noreply.github.com>
2024-09-21 16:56:57 -0500

    created components

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
-
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
