2184bf75e5c98d5553f240810f0cdb6a259753ba
Enlai <enleyee@gmail.com>
2024-09-21 14:12:50 -0500

    init amplify project

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
+// import { Predictions } from "aws-amplify/predictions";
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
+
+// Amplify.configure({
+//   ...Amplify.getConfig(),
+//   Predictions: config.custom.Predictions,
+// });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
