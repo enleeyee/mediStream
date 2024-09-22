ad4db826ec8e5868ea1ccddd27e71576b6ae71e5
Enlai <enleyee@gmail.com>
2024-09-21 21:30:00 -0500

    Revert "created components"
    This reverts commit f1034c446644ea6bb769538a58815411ef19bb4d.
    
    Back to normal

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
+
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
